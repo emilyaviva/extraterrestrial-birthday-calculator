36a8c66f42c908231bef544da5f3dd4d3185de56
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2015-09-08 22:36:14 -0700

    removed pikaday, added logic

--- app/js/client.js
+++ app/js/client.js
@@ -1,7 +1,50 @@
 'use strict';
 
+//var planets = require('../../models/planets.json');
+
+/* var $datepicker = $('#datepicker').pikaday({
+  firstDay: 0,
+  minDate: new Date(1900, 0, 1),
+  maxDate: new Date(2020, 12, 31),
+  yearRange: [1900, 2020]
+});
+
+*/
+
+var nextAnniv = function(date, planet) {
+  if (date._isAMomentObject && planet in planets) {
+    console.log(success);
+  }
+};
+
+var validateDate = function(arr) {
+  var year = arr[0];
+  var month = arr[1];
+  var day = arr[2];
+  if (month in [3, 5, 8, 10] && day > 30) return false;
+  if (month === 1 && day > 28) {
+    var febDate = new Date(year, month, day);
+    if (febDate.toString().slice(4,7) !== 'Feb') return false;
+  }
+  return true;
+};
+
+moment().format();
+// $datepicker.pikaday('show').pikaday('thisMonth');
+
 $('#input-date').on('submit', function(event) {
   event.preventDefault();
-  var userDate = ('#input-date').val();
-  $('#earth-date').html(userDate);
+  var inputArray = [parseInt($('#year').val()), parseInt($('#month').val()), parseInt($('#day').val())];
+  if (validateDate(inputArray)) {
+    var earthDate = inputMoment.format('dddd, MMMM D, YYYY');
+    $('#earth-date').html(earthDate);
+    var earthAge = moment.duration(moment().diff(inputMoment, 'years', true), 'years');
+    var earthYears = earthAge._data.years;
+    var earthMonths = Math.floor(earthAge._data.months) || null;
+    $('#earth-age').html(earthMonths ? earthYears + ' years ' + earthMonths + ' months' : earthYears + ' years');
+  } else {
+    alert('Not a valid date');
+  }
 });
+
+// nextAnniv(moment(), 'venus');
--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,24 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var sass = require('gulp-sass');
+var bootlint = require('gulp-bootlint');
 
-gulp.task('lint', function() {
+gulp.task('lint:js', function() {
   return gulp
     .src(['app/**/*.js', 'models/**/*.json', './gulpfile.js'])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
+gulp.task('lint:bootstrap', function() {
+  return gulp
+    .src('app/index.html')
+    .pipe(bootlint());
+});
+
 gulp.task('copy', function() {
   return gulp
-    .src('app/**/*.html')
+    .src(['app/**/*.html', 'app/js/**/*'])
     .pipe(gulp.dest('build/'));
 });
 
@@ -29,6 +36,8 @@ gulp.task('sass', function() {
 gulp.task('sass:watch', function() {
   gulp.watch('app/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('lint', ['lint:js', 'lint:bootlint']);
 
 gulp.task('build', ['sass', 'copy']);
 
